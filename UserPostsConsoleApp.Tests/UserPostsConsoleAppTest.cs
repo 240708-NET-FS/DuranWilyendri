6b18e6cdb4e2c09dc5ba86a5f579c2e1e686bed9
Wilyendri <wilyendri@gmail.com>
2024-07-15 16:01:17 -0400

    First files

---
+++ UserPostsConsoleApp.Tests/UserPostsConsoleAppTest.cs
@@ -0,0 +1,15 @@
+using UserPostsConsoleApp;
+using Xunit;
+using System;
+using System.IO;
+
+namespace UserPostsConsoleApp.Tests;
+
+public class UserPostsConsoleAppTest
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal("Hello Test", UserPostsApp.GetAString());
+    }
+}
---
+++ UserPostsConsoleApp/UserPostsApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace UserPostsConsoleApp;
+
+public class UserPostsApp {
+    public static void Main(string[] args) {
+
+    }
+
+    public static string GetAString() {
+
+        return "Hello Test";
+    }
+}
+
